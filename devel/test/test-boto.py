178e3d148672167bab5bd485cfbc7c525adf2fa5
denis lussier <denis@lussier.io>
2021-02-08 21:39:03 +0000

    wip security groups

--- devel/test/test-boto.py
+++ devel/test/test-boto.py
@@ -1,8 +1,25 @@
 import boto3, jmespath
 
 ec2 = boto3.client('ec2')
-response = ec2.describe_instances(InstanceIds=['i-0b30eef81610c55ef'])
-print(response)
+response = ec2.describe_security_groups()
+for sg in response['SecurityGroups']:
+  dict = {}
+  dict['name'] = sg['GroupName']
+  dict['id']  = sg['GroupId']
+  #print("DEBUG: sg.GroupName = " + str(sg['GroupName']))
+  for ipp in sg['IpPermissions']:
+    for r in ipp['IpRanges']:
+      dict['cidr'] = str(r['CidrIp'])
+      ##print(str(r['CidrIp']))
+      break
+    dict['port'] = str(ipp['FromPort']) + ":" + str(ipp['ToPort'])       
+    #print(str(ipp['FromPort']) + "\n")
+    break
 
-print(jmespath.search("Reservations[].Instances[].InstanceType", response))
-print(jmespath.search("Reservations[].Instances[].KeyName", response))
+  print(str(dict))
+
+#response = ec2.describe_instances(InstanceIds=['i-0b30eef81610c55ef'])
+#print(response)
+
+#print(jmespath.search("Reservations[].Instances[].InstanceType", response))
+#print(jmespath.search("Reservations[].Instances[].KeyName", response))
--- devel/test/test-openstack.py
+++ devel/test/test-openstack.py
@@ -4,7 +4,33 @@ openstack.enable_logging(debug=False)
 
 conn = openstack.connect(load_envvars=True)
 
-for server in conn.list_servers():
-   print(server)
+for sg in conn.list_security_groups():
+ try:
+   dict = {}
+   dict['name'] = sg.name
+   dict['id'] = sg.id
+   rules = sg.security_group_rules
+   port = None
+   for rule in rules:
+     if rule['direction'] == "ingress" and rule['protocol'] == "tcp":
+       ##print(str(rule))
+       dict['cidr'] = str(rule['remote_ip_prefix'])
+       dict['port'] = str(rule['port_range_min']) + ":" + str(rule['port_range_min'])
+       #dict['protocol'] = rule['protocol']
+       #dict['direction'] = rule['direction']
+       break
+     else:
+       continue
+#   dict['port'] = port
+#   dict['protocol'] = sg.protocol
+#   dict['direction'] = sg.direction
+   print(str(dict))
+ except Exception as e:
+   ##print("DEBUG: e = " + str(e))
+   continue
+   #print(sg)
+
+##for server in conn.list_servers():
+##   print(server)
 
 sys.exit(1)
