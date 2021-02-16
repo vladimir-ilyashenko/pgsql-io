9e2681146046a9bb31883407eb99eb1e6cda6cd9
denis lussier <denis@lussier.io>
2021-02-16 06:46:19 +0000

    make sure semantic_version & mistune are present

--- setupCLOUD.sh
+++ setupCLOUD.sh
@@ -12,8 +12,8 @@ else
 fi
 
 reqs="fire apache-libcloud python-dotenv parallel-ssh jmespath munch"
-reqs="$reqs CLICK paramiko psutil python-openstackclient boto3"
-reqs="$reqs mkdocs flask"
+reqs="$reqs CLICK python-openstackclient boto3"
+reqs="$reqs mkdocs flask semantic_version mistune"
 
 flags="--user --disable-pip-version-check --no-input"
 
