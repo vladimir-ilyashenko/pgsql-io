be2ce34180093f5e44d9f42855939c8081562bdf
denis lussier <denis@lussier.io>
2021-02-16 15:47:34 +0000

    wip

--- bp.sh
+++ bp.sh
@@ -21,12 +21,12 @@ cp startREST-API.sh $outp/.
 
 cd $outp
 
-./$api key insert $USER-key ubuntu ~/keys/$USER-key.pem
+./$api key insert $USER-pubkey ubuntu ~/keys/$USER-pubkey.pem
 ./$api key insert lussier-io-east2-key ubuntu ~/keys/lussier-io-east2-key.pem
 ./$api cloud create aws       west2 us-west-2
 ./$api cloud create aws       east2 us-east-2 --default-ssh-key=lussier-io-east2-key
-./$api cloud create openstack nnj2 --region=nnj2 --keys=$HOME/.openstack/nnj2.env --default-ssh-key=$USER-key
-./$api cloud create openstack nnj3 --region=nnj3 --keys=$HOME/.openstack/nnj3.env --default-ssh-key=$USER-key
+./$api cloud create openstack nnj2 --region=nnj2 --keys=$HOME/.openstack/nnj2.env --default-ssh-key=$USER-pubkey
+./$api cloud create openstack nnj3 --region=nnj3 --keys=$HOME/.openstack/nnj3.env --default-ssh-key=$USER-pubkey
 
 ./$api set GLOBAL REPO http://localhost:8000
 ./$api info
