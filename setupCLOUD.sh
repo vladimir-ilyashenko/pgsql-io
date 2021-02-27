472fa5715003ecc2d9c839eb6c3c00d492b8538e
denis lussier <denis@lussier.io>
2021-02-27 10:49:56 +0000

    wip setupCLOUD.sh

--- setupCLOUD.sh
+++ setupCLOUD.sh
@@ -1,29 +1,30 @@
 
 p3=python3
+ubu="Ubuntu 20.04"
 
-which yum > /dev/null 2>&1
+cat /etc/lsb-release | grep "$ubu" > /dev/null 2>&1
 rc=$?
-if [ "$rc" == "0" ]; then
-  echo "ERROR: Only tested on Ubuntu 20.04"
+if [ ! "$rc" == "0" ]; then
+  echo "ERROR: Only supported on $ubu"
   exit 1
-else
-  sudo apt update
-  sudo apt upgrade -y
-  INSTALL="sudo apt install -y"
-  $INSTALL sqlite3 net-tools
-  $INSTALL $p3-distutils $p3-psutil $p3-paramiko
 fi
 
-reqs="fire apache-libcloud python-dotenv parallel-ssh jmespath munch"
-reqs="$reqs CLICK flask semantic_version mistune"
-flags="--disable-pip-version-check --no-input"
-sudo pip3 install $flags $reqs
+sudo apt update
+sudo apt upgrade -y
+INSTALL="sudo apt install -y"
+$INSTALL sqlite3 net-tools rabbitmq-server mongodb
+$INSTALL $p3-celery $p3-pymongo
+$INSTALL $p3-distutils $p3-psutil $p3-paramiko
+$INSTALL $p3-mistune $p3-flask $p3-semantic-version 
+$INSTALL $p3-munch $p3-fire $p3-click
 
-sudo pip3 install $flags python-openstackclient boto3
-
-src/rabbitmq/install-rabbitmq
-src/mongodb/install-mongodb
-src/celery/install-celery
+sudo pip3 install --disable-pip-version-check --no-input --upgrade \
+apache-libcloud==3.3.1 \
+python-dotenv==0.15.0 \
+parallel-ssh==2.5.4 \
+jmespath==0.10.0 \
+boto3==1.17.17 \
+awscli==1.19.17
 
 echo "Goodbye!"
 exit 0
