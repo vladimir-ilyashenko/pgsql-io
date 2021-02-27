4de71313894c2a56d71e8bea578cc6b4dea0c397
denis lussier <denis.lussier>
2021-02-27 22:07:29 +0000

    1st pass at community rabbitmq

--- setupCLOUD.sh
+++ setupCLOUD.sh
@@ -9,8 +9,9 @@ if [ ! "$rc" == "0" ]; then
   exit 1
 fi
 
+sudo apt install -y curl wget gnupg sqlite3 net-tools 
+
 ## ADD REPO's & KEYS ##############################
-sudo apt-get install -y curl wget gnupg
 
 sudo add-apt-repository -y ppa:dqlite/stable
 
@@ -24,13 +25,18 @@ wget -qO - $url | sudo apt-key add -
 cmd="deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse"
 echo "$cmd" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
+sudo apt install apt-transport-https -y
+url=https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc |
+wget -O- $url | sudo apt-key add -
+url=https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
+wget -O- $url | sudo apt-key add -
+
 sudo apt update
 sudo apt upgrade -y
 INSTALL="sudo apt install -y"
 
-$INSTALL influxdb telegraf dqlite libdqlite-dev mongodb-org
+$INSTALL influxdb telegraf dqlite libdqlite-dev mongodb-org rabbitmq-server
 
-$INSTALL sqlite3 net-tools rabbitmq-server
 $INSTALL $p3-celery $p3-pymongo
 $INSTALL $p3-distutils $p3-psutil $p3-paramiko
 $INSTALL $p3-mistune $p3-flask $p3-semantic-version 
@@ -58,6 +64,7 @@ sudo systemctl enable --now telegraf
 sudo systemctl start telegraf
 
 sudo systemctl enable --now rabbitmq-server
+sudo rabbitmq-plugins enable rabbitmq_management
 sudo systemctl start rabbitmq-server
 
 # configure dqlite
