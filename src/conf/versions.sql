a62640646ad020c0e6d1415602ccc24a704fd4ea
denis lussier <denis@lussier.io>
2021-07-20 19:27:23 +0000

    wip

--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -268,9 +268,14 @@ INSERT INTO releases VALUES ('instantclient', 2, 'instantclient', 'Instant Clien
 INSERT INTO versions VALUES ('instantclient', '21.1', 'amd', 1, '20210317', '', '', '');
 
 INSERT INTO projects VALUES ('pgosql', 5, 0, 'hub', 0, 'https://github.com/pgsql-io/pgosql/releases',
-  'pgosql', 1, 'pgosql.png', 'Oracle PL/SQL<sup>&reg;</sup> Compatibility', 'https://github.com/pgsql-io/pgosql');
+  'pgosql', 1, 'pgosql.png', 'PL/SQL<sup>&reg;</sup> Compatibility', 'https://github.com/pgsql-io/pgosql');
 INSERT INTO releases VALUES ('pgosql-pg14', 2, 'pgosql', 'pgOSQL', '', 'test', '', 1, 'SSPL', '', '');
 INSERT INTO versions VALUES ('pgosql-pg14', '0.1',  'amd', 1, '20210720', 'pg14', '', '');
+
+INSERT INTO projects VALUES ('plusql', 5, 0, 'hub', 0, 'https://github.com/pgsql-io/plusql2/releases',
+  'plusql', 1, 'plusql.png', 'SQL*PLUS<sup>&reg;</sup> Compatible CLI', 'https://github.com/pgsql-io/plusql2');
+INSERT INTO releases VALUES ('plusql-pg14', 2, 'plusql', 'PlusQL', '', 'test', '', 1, 'SSPL', '', '');
+INSERT INTO versions VALUES ('plusql-pg14', '0.1',  'amd', 1, '20210720', 'pg14', '', '');
 
 INSERT INTO projects VALUES ('orafce', 5, 0, 'hub', 0, 'https://github.com/orafce/orafce/releases',
   'orafce', 1, 'larry.png', 'Ora Built-in Packages', 'https://github.com/orafce/orafce#orafce---oracles-compatibility-functions-and-packages');
