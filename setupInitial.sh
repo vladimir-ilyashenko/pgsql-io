ab31795d0402c92031798c2eb481333313ee0944
denis <denis@lussier.io>
2021-08-19 14:37:56 -0400

    use newer git on cos7

--- setupInitial.sh
+++ setupInitial.sh
@@ -45,6 +45,10 @@ if [ `uname` == 'Linux' ]; then
       sudo $yum hiredis-devel mysql-devel
     else
       ## CentOS 7 (used for stable & ...)  
+      sudo yum remove git
+      sudo yum -y install https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
+      sudo yum install git
+
       sudo yum -y install -y epel-release python-pip
       sudo yum -y groupinstall 'development tools'
       sudo yum -y install bison-devel libedit-devel zlib-devel bzip2-devel \
